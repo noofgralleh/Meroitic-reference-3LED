6b1ea489678981257205b5520831e7ef48e5f82e
noofgralleh <noofgralleh2012@gmail.com>
2025-10-06 18:05:36 +0300

    Create mani.ino

---
+++ mani.ino
@@ -0,0 +1,28 @@
+int redLed = 8;     // لمبة حمراء
+int yellowLed = 9;  // لمبة صفراء
+int greenLed = 10;  // لمبة خضراء
+
+void setup() {
+  // تعريف الأرجل كمخارج
+  pinMode(redLed, OUTPUT);
+  pinMode(yellowLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+}
+
+void loop() {
+  // 1- تشغيل الأحمر
+  digitalWrite(redLed, HIGH);
+  delay(5000); // 5 ثواني
+  digitalWrite(redLed, LOW);
+
+  // 2- تشغيل الأخضر
+  digitalWrite(greenLed, HIGH);
+  delay(5000); // 5 ثواني
+  digitalWrite(greenLed, LOW);
+
+  // 3- تشغيل الأصفر (تنبيه للتبديل)
+  digitalWrite(yellowLed, HIGH);
+  delay(2000); // ثانيتين
+  digitalWrite(yellowLed, LOW);
+}
+
